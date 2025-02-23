fa24f3756b0fa3b655c23be5434b4588522cd26b
mclvlc <89335488+mclvlc@users.noreply.github.com>
2025-02-23 20:12:07 +0800

    Update data.js
    peace我们可以和好嘛

--- data.js
+++ data.js
@@ -107,5 +107,23 @@ const websiteData = [
             { text: "官方认证", color: "#53D256" },{ text: "仓库", color: "#4274CD" }
         ]
     },
+    {
+        title: "我们可以和好嘛？",
+        description: "我们可以和好嘛？是一个求朋友或者恋人和好的网站,由依一开发",
+        url: "https://mclvlc.github.io/peace",
+        preview: "image/preview/peace.png",
+        tags: [
+            { text: "官方认证", color: "#53D256" },{ text: "表白类", color: "#FF69B4" }
+        ]
+    },
+    {
+        title: "我们可以和好嘛？",
+        description: "我们可以和好嘛？的仓库",
+        url: "https://github.com/mclvlc/peace",
+        preview: "image/preview/peace.png",
+        tags: [
+            { text: "官方认证", color: "#53D256" },{ text: "仓库", color: "#4274CD" }
+        ]
+    },
     // 添加新网站时只需复制这个对象格式
 ];
